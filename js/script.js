8e9c6e435d7e371be55e733d58e998389fd2253f
Florvin Bayaborda <61019061+Florvin-04@users.noreply.github.com>
2023-03-20 12:01:16 +0800

    challenge done (frontendmentor.io)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const hamburgerMenu = document.querySelector("[data-header-menu-btn]");
 const headerNav = document.querySelector("[data-header-navigation]");
-
+const body = document.querySelector("body");
 hamburgerMenu.addEventListener("click", () => {
   headerNav.classList.toggle("active-nav");
+  body.classList.toggle("active-nav");
 });
